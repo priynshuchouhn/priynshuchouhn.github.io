1fd3f250e85dc0a4817db6fa1c1469d2a4718da0
Priyanshu Chouhan <priyanschouhan123@gmail.com>
2023-07-24 00:36:51 +0530

    ANimation added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,44 @@
 // !########### For Date ###########
-const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 const d = new Date();
 let day = d.getDate();
 let month = months[d.getMonth()];
 let year = d.getFullYear();
 
-const date = document.getElementById('date');
-date.innerText = `${ day+'-'+month+'-'+year}`;
-
+const date = document.getElementById("date");
+date.innerText = `${day + "-" + month + "-" + year}`;
 
 $.ajax({
-    url: "https://geolocation-db.com/jsonp",
-    jsonpCallback: "callback",
-    dataType: "jsonp",
-    success: function(location) {
-        $('#location').html(location.city)
+  url: "https://geolocation-db.com/jsonp",
+  jsonpCallback: "callback",
+  dataType: "jsonp",
+  success: function (location) {
+    $("#location").html(location.city);
+  },
+});
+
+const observer = new IntersectionObserver((enteries) => {
+  enteries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }else{
+        entry.target.classList.remove("show");
     }
   });
+});
+
+const hiddenElement = document.querySelectorAll('.hidden');
+hiddenElement.forEach((el)=> observer.observe(el));
